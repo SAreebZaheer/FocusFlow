fec3bba81e3de97eb3b04c680cd31cc7009eca3b
SAreebZaheergit config --global user.name SAreebZaheer <sareebzaheer@gmail.com>
2025-02-22 13:12:09 +0500

    `Removed subproject FocusFlow and refactored ocr.py to use Google GenAI for image-to-text conversion.`

--- ocr.py
+++ ocr.py
@@ -1,13 +1,20 @@
 from google import genai
 from google.genai import types
+from datetime import datetime
 
 import PIL.Image
 
-image = PIL.Image.open('notes_images/images.png')
+def getText(filename):
+    filename = 'notes_images' + filename
+    image = PIL.image.open(filename)
+    image = PIL.Image.open(filename)
+    client = genai.Client(api_key="AIzaSyCPKQuDiibeF6Hud2ft3MyYOGMhMsuAOfQ")
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=["What is written in the image?Your response should  only contain the contents of the image and nothing else.", image])
+    text_name = "./notes/" + str(datetime.now()) + ".txt"
+    f = open(text_name, "a")
+    f.write(response.text)
+    f.close()
 
-client = genai.Client(api_key="AIzaSyCPKQuDiibeF6Hud2ft3MyYOGMhMsuAOfQ")
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents=["What is this image?", image])
-
-print(response.text)
+getText("Areeb")
