32d898849f8e53d678642f924a25fd31fb4001f7
Syed Areeb Zaheer <158595070+SAreebZaheer@users.noreply.github.com>
2025-02-22 15:04:22 +0500

    `Deleted two Python files: ocr.py and summarise.py`

--- ai.py
+++ ai.py
@@ -2,12 +2,14 @@ from google import genai
 from google.genai import types
 from datetime import datetime
 
+client = genai.Client(api_key="")
+
 import PIL.Image
 
 def getText(filename):
     filename = 'notes_images' + filename
     image = PIL.image.open(filename)
-    client = genai.Client(api_key="")
+    
     response = client.models.generate_content(
         model="gemini-2.0-flash",
         contents=["What is written in the image?Your response should  only contain the contents of the image and nothing else.", image])
@@ -15,5 +17,15 @@ def getText(filename):
     f = open(text_name, "a")
     f.write(response.text)
     f.close()
+    return  text_name
 
-getText("Areeb")
+def summarise(filename):
+    response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=["Summarise the following text to 10 Lines:"])
+    text_name = "./UI/notes/" + str(datetime.now()) + ".txt"
+    f = open(text_name, "a")
+    f.write(response.text)
+    f.close()
+    return  text_name
+    
